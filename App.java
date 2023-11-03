b7ca0004376b1b63df4f8ee685c40f8bbe08c77d
Bgirbert <149525570+Bgirbert@users.noreply.github.com>
2023-11-03 15:08:26 +0200

    App.java

--- App.java
+++ App.java
@@ -1,3 +1,4 @@
+ // this program will display the hello word in the screen
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hello world");
