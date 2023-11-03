79ca0e96cfe9ba7c2b20e0884b0aaf6b55e76e60
Bgirbert <149525570+Bgirbert@users.noreply.github.com>
2023-11-03 16:50:50 +0200

    Add files via upload

---
+++ simpleif.java
@@ -0,0 +1,12 @@
+
+ // this is the program to check whether someone is young or old according to the age on 28-october-2023
+public class simpleif { //main class
+    public static void main(String[] args) { // main method
+        int year = 20; // variable declaration
+        if(year>=16){ // condition
+            System.out.println("YOU ARE OLD"); // output if condition is true
+        } else{
+            System.out.println("YOU ARE STILL YOUNG");// output if condition is false
+        }
+    }
+}   
