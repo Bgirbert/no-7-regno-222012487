4099b114ebef362222db5b95b991377d0a51627e
Bgirbert <149525570+Bgirbert@users.noreply.github.com>
2023-11-08 15:41:54 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+ class Main{
+double x =3+1;
+int y=6;
+double result=Math.pow(y,x);
+
+public static void main(String[]args){
+    Main myRU=new Main();
+    System.out.println(myRU.result);
+}
+ }
